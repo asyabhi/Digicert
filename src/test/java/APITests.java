d387181805291485f593c0e4d77c1d9d61bdbaf1
Abhitendra Singh <sabhitendra911@gmail.com>
2024-05-04 20:47:51 +0200

    Added API Tests

---
+++ src/test/java/APITests.java
@@ -0,0 +1,64 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class APITests {
+
+    String BASE_URL = "https://swapi.dev/api/";
+    String FILMS = "films";
+    int FILMS_COUNT = 6;
+    String DIRECTOR = "Richard Marquand";
+    String PRODUCER = "Gary Kurtz, Rick McCallum";
+
+    JsonPath json;
+
+
+    @Test
+    public void getMovieCount() {
+        Response response = given()
+                .when()
+                .get(BASE_URL + FILMS)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        json = response.jsonPath();
+        Assert.assertEquals(json.getInt("count"), FILMS_COUNT);
+    }
+
+    @Test
+    public void getThirdMovieDirector() {
+        Response response = given()
+                .pathParams("id", 3)
+                .when()
+                .get(BASE_URL + FILMS + "/{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        json = response.jsonPath();
+        Assert.assertEquals(json.getString("director"), DIRECTOR);
+
+    }
+
+    @Test
+    public void getFifthMovieProducers() {
+        Response response = given()
+                .pathParams("id", 5)
+                .when()
+                .get(BASE_URL + FILMS + "/{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        json = response.jsonPath();
+        Assert.assertNotEquals(json.getString("producer"), PRODUCER);
+    }
+
+}
--- src/test/java/stepDefinitions/StarWarsStepDefs.java
+++ src/test/java/stepDefinitions/StarWarsStepDefs.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -82,4 +83,6 @@ public class StarWarsStepDefs {
 
         driver.quit();
     }
+
+
 }
